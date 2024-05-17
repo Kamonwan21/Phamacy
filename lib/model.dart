dc76df37861764f53e38103428bce349d0057c29
Kamonwan21 <mindinmyheat@gmail.com>
2024-05-17 15:33:17 +0700

    first commit

--- lib/model.dart
+++ lib/model.dart
@@ -2,22 +2,42 @@ class Employee {
   final int Id;
   final String Empname;
   final String Password;
+  final String BirthDate;
+  final String Diagnosis;
+  final String Allergy;
+  final String NamePhamacy;
+  final String Instruction;
 
   const Employee({
     required this.Id,
     required this.Empname,
     required this.Password,
+    required this.BirthDate,
+    required this.Diagnosis,
+    required this.Allergy,
+    required this.NamePhamacy,
+    required this.Instruction,
   });
 
   factory Employee.fromJson(Map<String, dynamic> json) => Employee(
-    Id: json["Id"],
-    Empname: json["Empname"],
-    Password: json["Password"],
-  );
+        Id: json["Id"],
+        Empname: json["Empname"],
+        Password: json["Password"],
+        BirthDate: json["BirthDate"],
+        Diagnosis: json["Diagnosis"],
+        Allergy: json["Allergy"],
+        NamePhamacy: json["NamePhamacy"],
+        Instruction: json["Instruction"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "Id": Id,
-    "Empname": Empname,
-    "Password": Password
-  };
+        "Id": Id,
+        "Empname": Empname,
+        "Password": Password,
+        "BirthDate": BirthDate,
+        "Diagnosis": Diagnosis,
+        "Allergy": Allergy,
+        "NamePhamacy": NamePhamacy,
+        "Instruction": Instruction
+      };
 }
--- lib/page.dart
+++ lib/page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/api_handler.dart';
-
+import 'package:flutter_tts/flutter_tts.dart';
 import 'model.dart';
 
 class Mainpage extends StatefulWidget {
@@ -11,6 +11,16 @@ class Mainpage extends StatefulWidget {
 }
 
 class _MainpageState extends State<Mainpage> {
+  final FlutterTts flutterTts = FlutterTts();
+
+  textToSpeech(String text, String language) async {
+    await flutterTts.setLanguage(language);
+    await flutterTts.setPitch(1.0);
+    await flutterTts.setVolume(0.5);
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.speak(text);
+  }
+
   ApiHandler apiHandler = ApiHandler();
   late List<Employee> data = [];
   void getData() async {
@@ -33,18 +43,57 @@ class _MainpageState extends State<Mainpage> {
         backgroundColor: Colors.teal,
         foregroundColor: Colors.white,
       ),
+      bottomNavigationBar: MaterialButton(
+        color: Colors.teal,
+        textColor: Colors.white,
+        padding: const EdgeInsets.all(30),
+        onPressed: getData,
+        child: const Text("Refresh"),
+      ),
       body: Column(
         children: [
           ListView.builder(
             shrinkWrap: true,
             itemCount: data.length,
             itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: Text("${data[index].Id}"),
-                title: Text("${data[index].Empname}"),
+              return Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.teal,
+                        width: 2.0), // Border color and width
+                    borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                  ),
+                  child: ListTile(
+                    leading: Text("${data[index].Id}"),
+                    title: Text("${data[index].Empname}"),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("DOB : ${data[index].BirthDate}"),
+                        Text("ชื่อยา : ${data[index].NamePhamacy}"),
+                        Text("วิธีใช้ : ${data[index].Instruction}"),
+                        Text("Diagnosis : ${data[index].Diagnosis}"),
+                        Text("Allergy : ${data[index].Allergy}"),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.volume_up),
+                      onPressed: () {
+                        textToSpeech(data[index].Empname, "en-US");
+                        textToSpeech(data[index].BirthDate, "en-US");
+                        textToSpeech(data[index].NamePhamacy, "th-TH");
+                        textToSpeech(data[index].Instruction, "th-TH");
+                        textToSpeech(data[index].Diagnosis, "en-US");
+                        textToSpeech(data[index].Allergy, "en-US");
+                      },
+                    ),
+                  ),
+                ),
               );
             },
-          )
+          ),
         ],
       ),
     );
